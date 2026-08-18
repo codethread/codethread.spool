de1004ee507197f6e1faae9407b1d52d0295cb9b
codethread <adamhalldesigns@gmail.com>
2026-08-18 20:33:13 +0100

    feat: register next harness aliases

--- spools/config/src/ct/spools/codethread/config.clj
+++ spools/config/src/ct/spools/codethread/config.clj
@@ -5,6 +5,7 @@
   capabilities consumers get when they activate `codethread/config`."
   (:require [ct.spools.agent-run :as shuttle]
             [ct.spools.codethread.agents :as agents]
+            [ct.spools.codethread.agents-next :as agents-next]
             [ct.spools.codethread.devflow :as devflow]
             [ct.spools.codethread.help :as help]
             [millstrand.api.lifecycle.alpha :as lifecycle]))
@@ -24,5 +25,6 @@
 
 (shuttle/use-harnesses! harness-defs)
 (shuttle/use-aliases! alias-defs)
+(lifecycle/use-resource! agents-next/harness-next-runtime)
 (lifecycle/use-resource! batteries-help-transform)
 (lifecycle/use-seed! devflow-kanban-adapter-binding)
