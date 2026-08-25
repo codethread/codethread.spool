2af2579f1ef1494c5c12d98689f6f989c10b4af5
codethread <adamhalldesigns@gmail.com>
2026-08-25 08:47:52 +0100

    chore: adopt consolidated Millhouse workflow

--- .millstrand/init.clj
+++ .millstrand/init.clj
@@ -11,24 +11,11 @@
                  {:ns 'millhouse.spools.workflow
                   :spools ['millhouse.spools/workflow]
                   :required? true})
-(runtime/module! runtime :millhouse/spools-workflow-cli
-                 {:ns 'millhouse.spools.workflow.cli
+(runtime/module! runtime :millhouse/spools-workflow-providers
+                 {:ns 'millhouse.spools.workflow.spool
                   :spools ['millhouse.spools/workflow]
                   :after [:millhouse/spools-workflow]
                   :required? true})
-(runtime/module! runtime :millhouse/spools-millstrand-workflows
-                 {:ns 'millhouse.spools.millstrand-workflows
-                  :spools ['millhouse.spools/millstrand-workflows
-                           'millhouse.spools/workflow]
-                  :after [:millhouse/spools-workflow]
-                  :required? true})
-(runtime/module! runtime :millhouse/spools-shell
-                 {:ns 'millhouse.spools.executors.shell
-                  :spools ['millhouse.spools.executors/shell
-                           'millhouse.spools/workflow]
-                  :after [:millhouse/spools-workflow]
-                  :required? true})
-
 (runtime/module! runtime :millhouse/spools-identity
                  {:ns 'millhouse.spools.identity
                   :spools ['millhouse.spools/identity]
